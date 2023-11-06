54285fb128f3ee0c113d6ad9e253af0e1945b03a
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-11-06 02:44:47 +0000

    added Elevated Button

---
+++ lib/components/my_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  VoidCallback onPressed;
+
+  MyButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(Colors.cyan[600]),
+        foregroundColor: const MaterialStatePropertyAll(Colors.black),
+      ),
+      child: Text(text),
+    );
+  }
+}
