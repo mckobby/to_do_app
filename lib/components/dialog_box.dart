7c3948d4cac8e4885ce3bf515dcab6d625e357c6
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-11-06 02:40:46 +0000

    added Dialog Box component

---
+++ lib/components/dialog_box.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../components/my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+  DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return AlertDialog(
+      backgroundColor: Colors.cyan[300],
+      content: SizedBox(
+        height: height * 0.2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Get user input
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Add a new task',
+              ),
+            ),
+            // Buttons => save + cancel
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // Save button
+                MyButton(
+                  text: 'Save',
+                  onPressed: onSave,
+                ),
+                SizedBox(
+                  width: width * 0.02,
+                ),
+                // Cancel button
+                MyButton(
+                  text: 'Cancel',
+                  onPressed: onCancel,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
