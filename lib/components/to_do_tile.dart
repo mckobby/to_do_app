fa35e1f483c31916ecfbfc3facd160c4ea829816
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-11-07 06:05:26 +0000

    added 'Slidable' widget to to_do_tile file

--- lib/components/to_do_tile.dart
+++ lib/components/to_do_tile.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
   ToDoTile({
     super.key,
     required this.taskName,
     required this.taskCompleted,
     required this.onChanged,
+    required this.deleteFunction,
   });
 
   @override
@@ -23,32 +26,45 @@ class ToDoTile extends StatelessWidget {
         right: width * 0.05,
         top: height * 0.02,
       ), // 25
-      child: Container(
-        padding: EdgeInsets.all(height * 0.02),
-        decoration: BoxDecoration(
-          color: Colors.cyan[600],
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
           children: [
-            // Checkbox
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              activeColor: Colors.black,
-            ),
-
-            // Task name
-            Text(
-              taskName,
-              style: TextStyle(
-                fontSize: 15,
-                decoration: taskCompleted
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-              ),
-            ),
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.red.shade300,
+              borderRadius: BorderRadius.circular(15),
+            )
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(height * 0.02),
+          decoration: BoxDecoration(
+            color: Colors.cyan[600],
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              // Checkbox
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+
+              // Task name
+              Text(
+                taskName,
+                style: TextStyle(
+                  fontSize: 15,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
