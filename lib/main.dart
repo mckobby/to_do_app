ae79e081cc4543bf310270fcf7abf910b0b64490
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-11-08 08:57:06 +0000

    initialized hive and opened a hive box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:to_do_app/pages/home_page.dart';
 
-void main() {
+void main() async {
+  // Initialize hive
+  await Hive.initFlutter();
+
+  // Open a box
+  var box = await Hive.openBox('mybox');
   runApp(const MyApp());
 }
 
